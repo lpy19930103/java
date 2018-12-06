39d7a6481dfe95245289750170d8f03b5004b295
lipengyuan <lipengyuan1993@gmail.com>
2018-12-06 17:35:55 +0800

    mybatis

---
+++ mybatis/src/main/java/com/lpy/mybatis/plugin/ExamplePlugin.java
@@ -0,0 +1,25 @@
+package com.lpy.mybatis.plugin;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+
+import java.util.Properties;
+
+@Intercepts({
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+})
+public class ExamplePlugin implements Interceptor {
+    public Object intercept(Invocation invocation) throws Throwable {
+        return invocation.proceed();
+    }
+
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    public void setProperties(Properties properties) {
+        System.out.println(properties.getProperty("someProperty"));
+
+    }
+}
